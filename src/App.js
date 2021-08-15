5ea7f5788b5487a53d843ae4686961315415dc20
Najah Bouriga <najah.bouriga@asm-dev.com>
2021-08-15 12:43:55 +0100

    add architectur

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+// Handle console logs
+import "utils/dropConsole";
+// Styles
+import "fontsource-roboto";
+// ROUTER
+import {BrowserRouter}   from "react-router-dom";
+import {RouterConfig}    from "navigation/RouterConfig";
+// MUI Theme
+import {ThemeProvider}   from "@material-ui/core";
+import {dark, light}     from "styles/muiTheme";
+import "./App.css";
+import {ProvideAuth}     from "navigation/Auth/ProvideAuth";
+// Redux
+import {Provider}        from "react-redux";
+import {store}           from "redux/store";
+import {ThemeSwitch}     from "./components/ThemeSwitch";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [darkState, setDarkState] = useState(false);
+    const handleThemeChange = () => {
+        setDarkState(!darkState);
+        console.log("theme=", darkState ? "dark" : "light");
+    };
+
+    return (
+        <>
+            <div>
+                <Provider store={store}>
+                    <ThemeProvider theme={darkState ? dark() : light()}>
+                        <ThemeSwitch
+                            darkState={darkState}
+                            handleThemeChange={handleThemeChange}
+                        />
+                        <ProvideAuth>
+                            <BrowserRouter>
+                                <RouterConfig/>
+                            </BrowserRouter>
+                        </ProvideAuth>
+                    </ThemeProvider>
+                </Provider>
+            </div>
+        </>
+    );
 }
 
 export default App;
